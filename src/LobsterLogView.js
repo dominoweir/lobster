4a863fa88e53fa98de2c95eb43a630fb7f4bdc06
Domino Weir <domino.weir@mongodb.com>
2020-03-31 12:44:37 -0700

    I think this is more what we're going for

--- src/LobsterLogView.js
+++ src/LobsterLogView.js
@@ -4,15 +4,16 @@ import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import type { ContextRouter, Match } from 'react-router-dom';
 import { lobster } from './reducers';
 import rootSaga from './sagas';
 import urlParse from './sagas/urlParse';
 import EvergreenLogViewer from './components/Fetch/EvergreenLogViewer';
 import LogkeeperLogViewer from './components/Fetch/LogkeeperLogViewer';
-import type { Line } from 'src/models';
 import { logger } from 'redux-logger';
 import { isProd } from './config';
+import type { EvergreenTaskLog } from './models';
+import * as actions from './actions';
 import './index.css';
 
 // Polyfills
@@ -32,42 +33,46 @@ const store = createStore(lobster, applyMiddleware(...middlewares));
 saga.run(urlParse);
 saga.run(rootSaga);
 
-const logviewer = (props) => (<LogkeeperLogViewer {...props} />);
+const logkeeperLogviewer = (props) => (<LogkeeperLogViewer {...props} />);
 const evergreenLogviewer = (props) => (<EvergreenLogViewer {...props} />);
-
-const Main = () => (
-  <main className="lobster">
-    <Switch>
-      <Route path="/lobster/build/:build/test/:test" render={logviewer} />
-      <Route path="/lobster/build/:build/all" render={logviewer} />
-      <Route exact path="/lobster/evergreen/task/:id/:execution/:type" render={evergreenLogviewer} />
-      <Route exact path="/lobster/evergreen/test/:id" render={evergreenLogviewer} />
-      <Route exact path="/lobster/evergreen/test/:id/:execution/:type" render={evergreenLogviewer} />
-      <Route path="/lobster/logdrop" render={logviewer} />
-    </Switch>
-  </main>
-);
 
 type Props = {
   url: string
 };
 
 type State = {
-  lines: Line[],
+  logType: string
 };
 
 export default class LobsterLogView extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = {
-      lines: []
+    const identity:EvergreenTaskLog = {
+      id: 'evergreen_ubuntu1604_test_model_patch_5e823e1f28baeaa22ae00823d83e03082cd148ab_5e4ff3abe3c3317e352062e4_20_02_21_15_13_48',
+      execution: 0,
+      log: 'T',
+      type: 'evergreen-task'
     };
+    store.dispatch(actions.loadLog(identity));
   }
 
   render() {
+    const match:Match = {
+      isExact: false,
+      params: {},
+      url: this.props.url,
+      path: ''
+    }
+    const ctx:ContextRouter = {
+      history: window.history,
+      location: window.location,
+      match: match
+    }
     return (
       <Provider store={store}>
-        <Main />
+        <React.Fragment>
+          { evergreenLogviewer(ctx) }
+        </React.Fragment>
       </Provider>
     );
   }
